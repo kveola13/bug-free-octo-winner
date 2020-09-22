2ae223fe552691a4d538d0acfd759df427a7ab77
olakv <kveola13@student.westerdals.no>
2020-09-22 12:32:41 +0200

    Init

---
+++ react/learning-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './Components/HelloWorld';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name="Ola" />
+    </div>
+  );
+}
+
+export default App;
