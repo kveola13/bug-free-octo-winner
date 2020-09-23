dc57b68e7019b009bc1600d65ecffffea617a6d3
olakv <kveola13@student.westerdals.no>
2020-09-23 08:48:51 +0200

    header added

--- react/learning-react/src/App.js
+++ react/learning-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
 import HelloWorld from './Components/HelloWorld';
 
 function App() {
   return (
     <div>
+      <Header />
       <HelloWorld name="Ola" />
       <CounterExample />
     </div>
