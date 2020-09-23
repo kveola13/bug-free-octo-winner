4d939e8526f273675e6c976651e815c75720e2c9
olakv <kveola13@student.westerdals.no>
2020-09-23 08:47:47 +0200

    refactoring

--- react/learning-react/src/App.js
+++ react/learning-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import CounterExample from './Components/CounterExample';
 import HelloWorld from './Components/HelloWorld';
 
 function App() {
   return (
     <div>
       <HelloWorld name="Ola" />
+      <CounterExample />
     </div>
   );
 }
