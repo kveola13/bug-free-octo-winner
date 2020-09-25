963ca40d73cb27490c976c0d74cea69a11e6765a
olakv <kveola13@student.westerdals.no>
2020-09-25 13:11:51 +0200

    added footer

---
+++ react/learning-react/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <footer className="border-b font-bold p-3">
+            &copy; Copyright Ola F.A Kveli
+        </footer>
+    )
+}
+
+export default Footer
