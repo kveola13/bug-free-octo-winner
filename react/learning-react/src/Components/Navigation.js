356bfbc5fa991b728e6dfe1d29b526c9cbece96a
olakv <kveola13@student.westerdals.no>
2020-09-28 13:55:53 +0200

    fix to nav

--- react/learning-react/src/Components/Navigation.js
+++ react/learning-react/src/Components/Navigation.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 function Navigation () {
     return(
     <nav>
-        
+        The nav
     </nav>
     )
 }
