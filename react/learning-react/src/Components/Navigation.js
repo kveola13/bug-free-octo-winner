c6153ccbc88871e22cdb3c23b181d2180759390e
olakv <kveola13@student.westerdals.no>
2020-09-29 08:22:36 +0200

    dependencies added

--- react/learning-react/src/Components/Navigation.js
+++ react/learning-react/src/Components/Navigation.js
@@ -7,10 +7,15 @@ function Navigation () {
     const [showMenu, setShowMenu] = useState(false)
 
     let menu
+    let menuMask
 
     if(showMenu){
-        menu = <div>
+        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
             The menu
+        </div>
+        
+        menuMask = <div className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+        onClick={() => setShowMenu(false)}>
         </div>
     }
 
@@ -21,6 +26,7 @@ function Navigation () {
                 icon={faBars} 
                 onClick={() => setShowMenu(!showMenu)}
             />
+            {menuMask}
             {menu}
         </span>
     </nav>
