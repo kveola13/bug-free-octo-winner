d9d5757e05388a9811305e411c708788080c09a9
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2020-12-06 18:51:00 +0100

    intro console logging

--- angular/intro/src/app/app.component.ts
+++ angular/intro/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+function log(target: any, name: any, descriptor: any){
+  console.log(target, name, descriptor)
+  const original = descriptor.value
+  descriptor.value = function(){
+    console.log("This function was hacked!")
+  }
+  return descriptor
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro';
+
+  constructor(){
+    this.aSimpleMethod()
+  }
+  
+  @log
+  aSimpleMethod(){
+    console.log("Hey there!")
+  }
 }
