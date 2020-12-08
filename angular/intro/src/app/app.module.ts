1ac4c55d58d7d2b8e1b67167471b10ff8522ff97
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2020-12-08 09:36:10 +0100

    init

--- angular/intro/src/app/app.module.ts
+++ angular/intro/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { RecordsService } from "./records.service";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
--- angular/intro/src/app/records.service.ts
+++ angular/intro/src/app/records.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class RecordsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    
+   }
 
   getData(){
     return [
