092a07d70ecc04bd297e24f0e174dc310f20d779
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2020-12-09 10:04:25 +0100

    css added

--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -13,7 +13,10 @@ export class HeroesComponent implements OnInit {
     name: "Windstorm"
   }
   heroes = HEROES
-  selectedHero: Hero | undefined
+  selectedHero: Hero = {
+    id: 1,
+    name: "Placeholder"
+  }
   constructor() { }
 
   ngOnInit(): void {
