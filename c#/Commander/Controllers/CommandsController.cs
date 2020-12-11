c702ce3c756ee42945380552a5e255772a9c12db
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2020-12-11 09:49:14 +0100

    init commander api

---
+++ c#/Commander/Controllers/CommandsController.cs
@@ -0,0 +1,31 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+        public CommandsController(ICommanderRepo commanderRepo)
+        {
+            _repository = commanderRepo;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            return Ok(_repository.GetAppCommands());
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            return Ok(_repository.GetCommandById(id));
+        }
+    }
+}
---
+++ c#/Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,26 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+            new Command { Id = 0, HowTo = "Jump", Line = "Just do it", Platform = "Real life" },
+            new Command { Id = 1, HowTo = "Roll", Line = "Just do it!", Platform = "Real life" },
+            new Command { Id = 2, HowTo = "Kick", Line = "Just kick it", Platform = "Real life" }
+            };
+            return commands;
+        }
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id=0, HowTo="Jump", Line="Just do it", Platform="Real life"};
+        }
+    }
+}
