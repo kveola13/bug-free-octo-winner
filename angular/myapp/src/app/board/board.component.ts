18bdf93af2840a61219ffbd1fab6102913fb3c20
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2020-12-14 09:20:07 +0100

    logic added

--- angular/myapp/src/app/board/board.component.ts
+++ angular/myapp/src/app/board/board.component.ts
@@ -30,6 +30,29 @@ export class BoardComponent implements OnInit {
       this.squares.splice(index, 1, this.player)
       this.xIsNext = !this.xIsNext
     }
+    this.winner = this.calculateWinner()
   }
 
+  calculateWinner() {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+    for (let index = 0; index < lines.length; index++) {
+      const [a,b,c] = lines[index]
+      if(
+        this.squares[a] && 
+        this.squares[a] === this.squares[b] && 
+        this.squares[a] === this.squares[c]){
+          return this.squares[a]
+        }
+    }
+    return null
+  }
 }
